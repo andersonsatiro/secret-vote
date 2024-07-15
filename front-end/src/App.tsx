1b35951818bc424246fcbae9f58322f63e338056
AndersonPaivaS <andersonpaiva135@gmail.com>
2024-07-15 16:25:13 -0300

    feat: create main titles of desktop version homepage

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,9 +1,15 @@
-import { GlobalHeader } from "./components/GlobalHeader";
+import { GlobalHeader } from "./components/GlobalHeader"
+import { MainHomeTitles } from "./components/MainHomeTitles"
 
 export function App() {
   return (
     <div className="w-full h-full min-h-screen bg-white100">
       <GlobalHeader navIsActive={true} />
+
+      <main className="flex items-center justify-center mt-24">
+        <MainHomeTitles />
+      </main>
+
     </div>
   )
 }
---
+++ front-end/src/components/MainHomeTitles.tsx
@@ -0,0 +1,25 @@
+import BrasilFlag from '../assets/brasil-flag.svg'
+
+export function MainHomeTitles(){
+  return(
+    <section className="flex flex-col items-center gap-5">
+
+      <h1 className="text-6xl text-gray950 text-center font-semibold leading-[4rem]">
+        <span className="text-purple300">Voto Secreto24: </span>
+        participe <br/> e veja os resultados
+      </h1>
+
+      <h2 className="flex text-3xl">
+        das principais disputas eleitorais do
+        <img src={BrasilFlag} alt="" className="w-8 ml-3" />
+        .
+      </h2>
+
+      <p className="text-gray400">
+        <span className="text-red100">Obs.:</span> os resultados desta enquete refletem
+        apenas a opinião dos participantes do site.
+      </p>
+
+    </section>
+  )
+}
